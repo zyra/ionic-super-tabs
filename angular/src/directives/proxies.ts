26a638c9a315cf625d27f01c52c07a3319cce8dc
Ibby Hadeed <ibby@zyra.ca>
2020-06-14 03:16:02 -0400

    chore: switch to official angular binding method

--- angular/src/directives/proxies.ts
+++ angular/src/directives/proxies.ts
@@ -1,68 +1,68 @@
-/* eslint-disable */
 /* tslint:disable */
 /* auto-generated angular directive proxies */
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, NgZone } from "@angular/core";
-import { ProxyCmp, proxyOutputs } from "./proxies-utils";
-import { Components } from "@ionic-super-tabs/core";
-export declare interface SuperTab extends Components.SuperTab {
-}
-@ProxyCmp({ inputs: ["noScroll"], "methods": ["getRootScrollableEl"] })
-@Component({ selector: "super-tab", changeDetection: ChangeDetectionStrategy.OnPush, template: "<ng-content></ng-content>", inputs: ["noScroll"] })
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, ElementRef, EventEmitter, NgZone } from '@angular/core';
+import { ProxyCmp, proxyOutputs } from './proxies-utils';
+
+import { Components } from '@ionic-super-tabs/core'
+
+export declare interface SuperTab extends Components.SuperTab {}
+@ProxyCmp({inputs: ['noScroll'], 'methods': ['getRootScrollableEl']})
+@Component({ selector: 'super-tab', changeDetection: ChangeDetectionStrategy.OnPush, template: '<ng-content></ng-content>', inputs: ['noScroll'] })
 export class SuperTab {
-    protected el: HTMLElement;
-    constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
-        c.detach();
-        this.el = r.nativeElement;
-    }
+  protected el: HTMLElement;
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = r.nativeElement;
+  }
 }
-export declare interface SuperTabButton extends Components.SuperTabButton {
-}
-@ProxyCmp({ inputs: ["disabled"] })
-@Component({ selector: "super-tab-button", changeDetection: ChangeDetectionStrategy.OnPush, template: "<ng-content></ng-content>", inputs: ["disabled"] })
+
+export declare interface SuperTabButton extends Components.SuperTabButton {}
+@ProxyCmp({inputs: ['disabled']})
+@Component({ selector: 'super-tab-button', changeDetection: ChangeDetectionStrategy.OnPush, template: '<ng-content></ng-content>', inputs: ['disabled'] })
 export class SuperTabButton {
-    protected el: HTMLElement;
-    constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
-        c.detach();
-        this.el = r.nativeElement;
-    }
+  protected el: HTMLElement;
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = r.nativeElement;
+  }
 }
-export declare interface SuperTabs extends Components.SuperTabs {
-}
-@ProxyCmp({ inputs: ["activeTabIndex", "config"], "methods": ["setConfig", "selectTab"] })
-@Component({ selector: "super-tabs", changeDetection: ChangeDetectionStrategy.OnPush, template: "<ng-content></ng-content>", inputs: ["activeTabIndex", "config"] })
+
+export declare interface SuperTabs extends Components.SuperTabs {}
+@ProxyCmp({inputs: ['activeTabIndex', 'config'], 'methods': ['setConfig', 'selectTab']})
+@Component({ selector: 'super-tabs', changeDetection: ChangeDetectionStrategy.OnPush, template: '<ng-content></ng-content>', inputs: ['activeTabIndex', 'config'] })
 export class SuperTabs {
-    tabChange!: EventEmitter<CustomEvent>;
-    protected el: HTMLElement;
-    constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
-        c.detach();
-        this.el = r.nativeElement;
-        proxyOutputs(this, this.el, ["tabChange"]);
-    }
+  tabChange!: EventEmitter<CustomEvent>;
+  protected el: HTMLElement;
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = r.nativeElement;
+    proxyOutputs(this, this.el, ['tabChange']);
+  }
 }
-export declare interface SuperTabsContainer extends Components.SuperTabsContainer {
-}
-@ProxyCmp({ inputs: ["autoScrollTop", "swipeEnabled"], "methods": ["scrollToTop"] })
-@Component({ selector: "super-tabs-container", changeDetection: ChangeDetectionStrategy.OnPush, template: "<ng-content></ng-content>", inputs: ["autoScrollTop", "swipeEnabled"] })
+
+export declare interface SuperTabsContainer extends Components.SuperTabsContainer {}
+@ProxyCmp({inputs: ['autoScrollTop', 'swipeEnabled'], 'methods': ['scrollToTop']})
+@Component({ selector: 'super-tabs-container', changeDetection: ChangeDetectionStrategy.OnPush, template: '<ng-content></ng-content>', inputs: ['autoScrollTop', 'swipeEnabled'] })
 export class SuperTabsContainer {
-    activeTabIndexChange!: EventEmitter<CustomEvent>;
-    selectedTabIndexChange!: EventEmitter<CustomEvent>;
-    protected el: HTMLElement;
-    constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
-        c.detach();
-        this.el = r.nativeElement;
-        proxyOutputs(this, this.el, ["activeTabIndexChange", "selectedTabIndexChange"]);
-    }
+  activeTabIndexChange!: EventEmitter<CustomEvent>;
+  selectedTabIndexChange!: EventEmitter<CustomEvent>;
+  protected el: HTMLElement;
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = r.nativeElement;
+    proxyOutputs(this, this.el, ['activeTabIndexChange', 'selectedTabIndexChange']);
+  }
 }
-export declare interface SuperTabsToolbar extends Components.SuperTabsToolbar {
-}
-@ProxyCmp({ inputs: ["color", "scrollable", "scrollablePadding", "showIndicator"] })
-@Component({ selector: "super-tabs-toolbar", changeDetection: ChangeDetectionStrategy.OnPush, template: "<ng-content></ng-content>", inputs: ["color", "scrollable", "scrollablePadding", "showIndicator"] })
+
+export declare interface SuperTabsToolbar extends Components.SuperTabsToolbar {}
+@ProxyCmp({inputs: ['color', 'scrollable', 'scrollablePadding', 'showIndicator']})
+@Component({ selector: 'super-tabs-toolbar', changeDetection: ChangeDetectionStrategy.OnPush, template: '<ng-content></ng-content>', inputs: ['color', 'scrollable', 'scrollablePadding', 'showIndicator'] })
 export class SuperTabsToolbar {
-    buttonClick!: EventEmitter<CustomEvent>;
-    protected el: HTMLElement;
-    constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
-        c.detach();
-        this.el = r.nativeElement;
-        proxyOutputs(this, this.el, ["buttonClick"]);
-    }
+  buttonClick!: EventEmitter<CustomEvent>;
+  protected el: HTMLElement;
+  constructor(c: ChangeDetectorRef, r: ElementRef, protected z: NgZone) {
+    c.detach();
+    this.el = r.nativeElement;
+    proxyOutputs(this, this.el, ['buttonClick']);
+  }
 }
--- core/stencil.config.ts
+++ core/stencil.config.ts
@@ -1,5 +1,7 @@
+import { angularOutputTarget } from '@stencil/angular-output-target';
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
+
 
 export const config: Config = {
 
@@ -23,10 +25,9 @@ export const config: Config = {
       strict: true,
     },
     {
-      type: 'dist-hydrate-script'
+      type: 'dist-hydrate-script',
     },
-    {
-      type: 'angular',
+    angularOutputTarget({
       componentCorePackage: '@ionic-super-tabs/core',
       directivesProxyFile: '../angular/src/directives/proxies.ts',
       directivesUtilsFile: '../angular/src/directives/proxies-utils.ts',
@@ -34,7 +35,7 @@ export const config: Config = {
       excludeComponents: [
         'super-tab-indicator',
       ],
-    },
+    }),
   ],
   validateTypes: true,
 };
